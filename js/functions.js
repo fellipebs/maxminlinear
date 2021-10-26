f1c243b7503ca1314d0f300cbf3fcba65939140b
Fellipe Saraiva <fellipe.saraiva@netscanner.com.br>
2021-10-26 20:17:58 -0300

    att metodo minimizacao

--- js/functions.js
+++ js/functions.js
@@ -86,7 +86,7 @@ function maximizacao(){ // Função para processo de maximização
     //Imprimindo a tabela atual
     imprimir(matriz, linhas);
 
-    while(paraLoop(matriz[matriz.length-1],vezesRodou)){ // Passando linha Z como parametro e vezes que rodou.
+    while(paraLoopMaximizacao(matriz[matriz.length-1],vezesRodou)){ // Passando linha Z como parametro e vezes que rodou.
         
         //Escolhendo o termo de menor valor na linha z
         var valorminimo = Math.min(...matriz[3]);
@@ -216,9 +216,116 @@ function minimizacao(){ // Função para processo de minimização
     //Imprimindo a tabela atual
     imprimir(matriz, linhas);
 
+    while(paraLoopMinizacao(matriz,vezesRodou)){ // Passando linha Z como parametro e vezes que rodou.
+
+    //Escolhendo o termo de menor valor na linha RS
+    var valorminimo = Number.MAX_VALUE; // Variavel que guardará o valor minimo 
+    var linhaminimo = ""; // Linha que será a escolhida
+    for(var i = 0; i < matriz.length - 1; i++){
+        if(matriz[i][matriz[0].length - 1] < valorminimo){
+            valorminimo = matriz[i][matriz[0].length - 1];
+            linhaminimo = i;
+        } 
+    }
+    imprimirTexto("O menor valor da coluna RS é " + valorminimo.toFixed(2) + ", sua linha foi escolhida.");
+
+    // criando vetor que irá receber os valores divididos
+    var vetAux = [];
+
+    for(var i = 0; i < matriz[linhaminimo].length; i++){
+        if(matriz[linhaminimo][i] == 0 )
+            vetAux.push(0);
+        else{
+            // forçando o resultado a ser sempre prositivo
+            resultado = matriz[matriz.length - 1][i] / matriz[linhaminimo][i];
+            if(resultado < 0)
+                resultado = resultado * - 1;
+
+            vetAux.push(resultado); // jogando resultados em vetor auxiliar
+        }
+    }
+
+  
+    //For para saber o indice do menor resultado
+    var indiceColuna = "";
+    var valorminimo2 = Number.MAX_VALUE; // Variavel que guardará o valor minimo 
+    for(var i = 0; i < vetAux.length; i++){
+        if(vetAux[i] < valorminimo2 && vetAux[i] != 0){
+            valorminimo2 = vetAux[i];
+            indiceColuna = i;
+        }
+    }
+
+    console.log(indiceColuna);
+    var textoLinha = "A coluna escolhida ("+ (indiceColuna+1) +") é: \n";
+
+    // Loop para escrever a linha
+    for(var i = 0; i < matriz.length; i++){
+        textoLinha += "  "+(matriz[i][indiceColuna]).toFixed(2)+"\n";
+    }
+
+    //Imprimindo a coluna que será utilizada
+    imprimirTexto(textoLinha);
+
+    //Escolhendo o pivô
+    var pivo = matriz[linhaminimo][indiceColuna];
+    imprimirTexto("O pivô escolhido é: " + pivo.toFixed(2));
+
+    //Modificando a linha do pivô
+    for(var i = 0; i < matriz[linhaminimo].length; i++){
+        matriz[linhaminimo][i] = matriz[linhaminimo][i]/pivo;
+    }
+
+    //Imprimindo matriz após modificar a linha do pivô
+    imprimir(matriz, linhas);
+
+    var pivo = matriz[linhaminimo][indiceColuna];
+    imprimirTexto("Pivô após contas é: " + pivo);
+
+    // valor base da linha que será utilizado para a conta.
+    var valorbase;
+    //Modificando as demais linhas
+    for(var i = 0; i < linhas + 1; i++){
+        var valorbaseaux = matriz[i][indiceColuna];
+        if (i != linhaminimo){
+            for(var j = 0; j < linhas + 3; j++){ 
+                valorbase = matriz[linhaminimo][j]; // Valor da linha que já foi modificada
+                matriz[i][j] = matriz[i][j]-(valorbaseaux*valorbase); //Adicionando novo valor na matriz, já calculado
+            }
+        }
+    }
+
+    //Fim do loop
+    //Imprimindo a tabela atual
+    imprimir(matriz, linhas);
+    vezesRodou++; // Controlador para evitar loop infinito
+
+    }
+
+    if(vezesRodou < 49){
+        var textoFinal = "";
+        for(var i = 0; i < matriz.length; i++){
+            if(matriz[i][0] == 1){
+                textoFinal += "X é: "+ matriz[i][linhas + 2].toFixed(2)+"\n";
+            }
+
+            if(matriz[i][1] == 1){
+                textoFinal += "  Y é: "+ matriz[i][linhas + 2].toFixed(2) + "\n";
+            }
+
+            if(i == matriz.length - 1){
+                zfinal = matriz[i][matriz[0].length - 1];
+                if(zfinal < 0)
+                    zfinal = zfinal * -1;
 
 
-    
+                textoFinal += "  Z é: "+ zfinal.toFixed(2) + "\n"; 
+            }
+        } 
+
+        imprimirTexto(textoFinal);
+    }
+
 }
 
 // Imprime a Matriz do sistema por completo!
@@ -243,8 +350,8 @@ function imprimirTexto(texto){
     $('#resultado').html($('#resultado').html() + "\n-----------------------------------------------------------------------------------------------------------------\n");
 }
 
-// Método bool para parar o loop, entre com a linha Z e ele retornará se existem valores negativos
-function paraLoop(matriz, vezesRodou){
+// Método bool para parar o loop, entre com a linha Z e ele retornará se existem valores negativos - Maximização
+function paraLoopMaximizacao(matriz, vezesRodou){
 
     if(vezesRodou > 49){
         imprimirTexto("A função 50 vezes, por isso a execução será abortada, evitando loop infinito.");
@@ -254,6 +361,23 @@ function paraLoop(matriz, vezesRodou){
     for(var i = 0; i < matriz.length; i++){
         if(matriz[i] < 0)
             return true;
+    }
+
+    return false;
+}
+
+// Método bool para parar o loop, entre com a matriz e ele retornará se a coluna RS possui valores negativos - Minimização
+function paraLoopMinizacao(matriz, vezesRodou){
+
+    if(vezesRodou > 49){
+        imprimirTexto("A função 50 vezes, por isso a execução será abortada, evitando loop infinito.");
+        return false;
+    }    
+
+    for(var i = 0; i < matriz.length - 1; i++){
+        if(matriz[i][matriz[0].length - 1] < 0){
+            return true;
+        } 
     }
 
     return false;
